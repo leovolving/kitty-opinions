62c643de61c1486516556bc4f1a9fe7b5da057fa
ljyockey <linyockey@gmail.com>
2017-04-07 09:25:17 -0700

    added photo to search-fail and aded background color to form

--- js/app.js
+++ js/app.js
@@ -87,7 +87,8 @@ function displaySearchResults(data) {
 		})
 	}
 	else {
-		searchResults += '<p id="search-fail">No results. Check your spelling, human. -_-</p>';
+		searchResults += '<p id="search-fail">No results. Check your spelling, human. -_-</p>' +
+		'<a href="http://thecatapi.com/?id=75a" target="blank"><img id="search-fail-image" src="http://thecatapi.com/api/images/get?id=75a"></a>';
 	}
 	$('.search-results').html(searchResults);
 }
